41fd4b50cf4ceff8cb950e77d98d6cc31ecb39e2
Adler <adlered@outlook.com>
2020-01-16 17:36:39 +0800

    Initial commit

---
+++ src/pers/adlered/s1970/Main.java
@@ -0,0 +1,13 @@
+package pers.adlered.s1970;
+
+/**
+ * @author https://github.com/AdlerED
+ * @date 2020/1/16 17:27
+ * @version 1.0
+ * <p>高性能、精简的控制台图形框架</p>
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
